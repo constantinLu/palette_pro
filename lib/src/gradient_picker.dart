4534edf6f4f8cd44103249e93d35b04008ee9d08
Constantin Lungu <lunguucatalin@gmail.com>
2023-08-02 18:36:10 +0300

    Initial commit

---
+++ lib/src/gradient_picker.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/painting.dart';
+
+const pinkpurple = LinearGradient(
+  begin: Alignment.centerLeft,
+  end: Alignment.centerRight,
+  colors: [Color(0XFFaa367c), Color(0XFF4a2fbd)],
+);
+
+const grayBack = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0XFF2E2D36), Color(0XFF11101D)],
+);
+const grayWhite = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFFFFF), Color(0xFFF3F2FF)],
+);
+
+const buttonGradi = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [Color(0XFF7DE7EB), Color(0XFF33BBCF)],
+);
+
+const contactGradi = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [Color(0XFF2E2D36), Color(0XFF11101D)],
+);
---
+++ lib/src/palette_pro_base.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/painting.dart';
+
+///whites
+const whiteFull = Color.fromRGBO(255, 255, 255, 1);
+const whiteSnow = Color.fromRGBO(248, 248, 247, 1);
+const kcWhiteCultured = Color.fromRGBO(221, 221, 216, 1);
+
+///blacks
+const kcBlackCarbon = Color(0xff252525);
+const kcDarkGreyColor = Color(0xFF121212);
+const kcDarkGreyShimmerEndColor = Color(0xFF1C1C1C);
+const kcMediumGrey = Color(0xFF222222);
+
+const blackFull = Color.fromRGBO(0, 0, 0, 1);
+const blackErie = Color.fromRGBO(37, 37, 37, 1);
+const blackJet = Color.fromRGBO(51, 51, 51, 1);
+const blackCard = Color.fromRGBO(13, 13, 13, 1);
+const blackCard2 = Color.fromRGBO(16, 16, 16, 1);
+
+///greys
+const Color kcVeryLightGrey = Color(0xFFE3E3E3);
+const Color kcLightGrey = Color(0xff989898);
+const Color kcTitleGrey = Color(0xff949494);
+
+const greyPlatinum = Color.fromRGBO(228, 228, 228, 1);
+const greySpanish = Color.fromRGBO(148, 148, 148, 1);
+const greyDim = Color.fromRGBO(106, 106, 106, 1);
+const Color greySpanishHexa = Color(0xCC949494);
+
+///colours
+const Color kcTitleTurquoise = Color(0xff03BFB5);
+const Color kcPrimaryColor = Color(0xFF9600FF);
+const Color kcPrimaryColorDark = Color(0xFF300151);
